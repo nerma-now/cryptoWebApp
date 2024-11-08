350af0fa918203c4f9c227e5cf63cb09aa82d191
nerma-now <n.ermak08@yandex.by>
2024-11-08 17:46:19 +0300

    Change exception CMCHTTPClient

--- src/http_client.py
+++ src/http_client.py
@@ -17,37 +17,45 @@ class CMCHTTPClient(HTTPClient):
     @alru_cache(ttl=5)
     async def get_listings(self):
         async with self._session.get('/v1/cryptocurrency/listings/latest') as response:
-            result = await response.json()
-            await self._session.close()
-            print(response.status)
-            return result['data']
+            try:
+                result = await response.json()
+                await self._session.close()
+                return result['data']
+            except Exception as ex:
+                return print(ex, response.status)
 
     @alru_cache(ttl=5)
     async def get_currency_logo(self, id: int):
         async with self._session.get('/v2/cryptocurrency/info',
                                      params={'id': id}
                                      ) as response:
-            result = await response.json()
-            await self._session.close()
-            print(response.status)
-            return { 'logo': result['data'][str(id)]['logo'] }
+            try:
+                result = await response.json()
+                await self._session.close()
+                return { 'logo': result['data'][str(id)]['logo'] }
+            except Exception as ex:
+                return print(ex, response.status)
 
     @alru_cache(ttl=5)
     async def get_currency_info(self, id: int):
         async with self._session.get('/v2/cryptocurrency/info',
                                      params={'id': id}
                                      ) as response:
-            result = await response.json()
-            await self._session.close()
-            print(response.status)
-            return result['data'][str(id)]
+            try:
+                result = await response.json()
+                await self._session.close()
+                return result['data'][str(id)]
+            except Exception as ex:
+                return print(ex, response.status)
 
     @alru_cache(ttl=5)
     async def get_currency(self, id: int):
         async with self._session.get('/v2/cryptocurrency/quotes/latest',
                                     params={'id': id}
                                      ) as response:
-            result = await response.json()
-            await self._session.close()
-            print(response.status)
-            return result['data'][str(id)]
+            try:
+                result = await response.json()
+                await self._session.close()
+                return result['data'][str(id)]
+            except Exception as ex:
+                print(ex, response.status)
